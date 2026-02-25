869a64d82b421c09bdee90c5941707c38830baa6
LOC-DEV71 <loclc8533@ut.edu.vn>
2026-02-25 21:36:16 +0700

    update helper/sendMail

--- helper/sendMail.js
+++ helper/sendMail.js
@@ -4,13 +4,16 @@ module.exports.sendMail = (email, subject, html) =>{
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: '1945musictrending@gmail.com',         
-            pass: 'pxme hqyl ngra bhij'    
+            // user: '1945musictrending@gmail.com',         
+            // pass: 'pxme hqyl ngra bhij'    
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS
         }
     });
 
     const mailOptions = {
-        from: '1945musictrending@gmail.com',
+        // from: '1945musictrending@gmail.com',
+        from: process.env.MAIL_USER,
         to: email,
         subject: subject,
         html: html
