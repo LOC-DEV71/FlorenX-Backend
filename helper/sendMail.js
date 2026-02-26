f14fe1f70c198d3aaad81d60dddd2791a23537b9
LOC-DEV71 <loclc8533@ut.edu.vn>
2026-02-26 22:57:17 +0700

    add resend

--- helper/sendMail.js
+++ helper/sendMail.js
@@ -1,49 +1,49 @@
 // RAILWAY KO CHO PHÉP SMTP (block outbound mail)
-const nodemailer = require('nodemailer');
-module.exports.sendMail = (email, subject, html) =>{
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {    
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS
-        }
-    });
+// const nodemailer = require('nodemailer');
+// module.exports.sendMail = (email, subject, html) =>{
+//     const transporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         auth: {    
+//             user: process.env.MAIL_USER,
+//             pass: process.env.MAIL_PASS
+//         }
+//     });
 
-    const mailOptions = {
-        from: process.env.MAIL_USER,
-        to: email,
-        subject: subject,
-        html: html
-    };
+//     const mailOptions = {
+//         from: process.env.MAIL_USER,
+//         to: email,
+//         subject: subject,
+//         html: html
+//     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log("Error:", error);
-        } else {
-            console.log('Email sent:', info.response);
-        }
-    });
+//     transporter.sendMail(mailOptions, (error, info) => {
+//         if (error) {
+//             console.log("Error:", error);
+//         } else {
+//             console.log('Email sent:', info.response);
+//         }
+//     });
 
-}
+// }
 
 
 
 // fix cho railway tạm
-// const { Resend } = require("resend");
+const { Resend } = require("resend");
 
-// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-// module.exports.sendMail = async (email, subject, html) => {
-//   try {
-//     const data = await resend.emails.send({
-//       from: "FlorenX <onboarding@resend.dev>",
-//       to: email,
-//       subject,
-//       html
-//     });
+module.exports.sendMail = async (email, subject, html) => {
+  try {
+    const data = await resend.emails.send({
+      from: "FlorenX <onboarding@resend.dev>",
+      to: email,
+      subject,
+      html
+    });
 
-//     console.log("Email sent:", data);
-//   } catch (err) {
-//     console.error("Send mail error:", err);
-//   }
-// };
+    console.log("Email sent:", data);
+  } catch (err) {
+    console.error("Send mail error:", err);
+  }
+};
